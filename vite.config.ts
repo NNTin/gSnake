b626442fd245ec435f61df820042c740ec3fe13c
Tin Nguyen <tnguyen@itq.lan>
2026-01-13 20:30:17 +0100

    automatic page deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// Declare process to avoid TypeScript errors since @types/node is not installed
+declare const process: { env: Record<string, string | undefined> }
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: process.env.VITE_BASE_PATH || '/',
 })
